b202dfdc91054ab23d765939f0f941cae9727558
Rom_work <work@email.com>
2025-11-28 12:12:59 +0200

    k

--- practice_chat/practice_palindrom.py
+++ practice_chat/practice_palindrom.py
@@ -1,10 +1,9 @@
 
-
 def palindrom(word):
     if word == word[::-1]:
         print('+')
     else:
         print('-')
 
-palindrom('ama')      # +
-palindrom('amnyya')   # -
+palindrom('ama')
+palindrom('amnyya')
